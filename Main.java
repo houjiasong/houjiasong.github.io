06e2a90d759119b9eb6871514f0b06a0991cb702
houjiasong <houjiasong@kuaishou.com>
2024-08-10 18:40:35 +0800

    1

--- Main.java
+++ Main.java
@@ -1,56 +1,35 @@
-import java.util.*;
-
-public class Main {
-    public static int n, m, k;
-    public static int[] w, v;
-    public static boolean[][] excl;
-    public static int maxVal = 0;
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int k = scanner.nextInt();
-        w = new int[n];
-        v = new int[n];
-        excl = new boolean[n][n];
-
-        for (int i = 0;i < n; i++) {
-            w[i] = scanner.nextInt();
-            v[i] = scanner.nextInt();
-        }
-
-        for (int i = 0;i < k; i++) {
-            int a = scanner.nextInt() - 1;
-            int b = scanner.nextInt() - 1;
-            excl[a][b] = true;
-            excl[b][a] = true;
-        }
-
-        dfs(0,0,0, new boolean[n]);
-        System.out.println(maxVal);
-    }
-
-    public static void dfs(int i, int curW, int curVal, boolean[] picked) {
-        if (i == n) {
-            maxVal = Math.max(maxVal, curVal);
-            return;
-        }
-        dfs(i+1, curW, curVal, picked);
-        if (curW + w[i] <= m && !ban(i,picked)) {
-            picked[i] = true;
-            dfs(i+1, curW + w[i], curVal + v[i], picked);
-            picked[i] = false;
-        }
-
-    }
-
-    public static boolean ban(int item, boolean[] picked) {
-        for (int i = 0; i < n; i++) {
-            if (picked[i] && excl[item][i]) {
-                return true;
+ExecutorService executor = Executors.newFixedThreadPool(5);
+ListeningExecutorService guavaExecutor = MoreExecutors.listeningDecorator(executor);
+ListenableFuture<String> future1 = guavaExecutor.submit(() -> {
+    //step 1
+    System.out.println("执行step 1");
+    return "step1 result";
+});
+ListenableFuture<String> future2 = guavaExecutor.submit(() -> {
+    //step 2
+    System.out.println("执行step 2");
+    return "step2 result";
+});
+ListenableFuture<List<String>> future1And2 = Futures.allAsList(future1, future2);
+Futures.addCallback(future1And2, new FutureCallback<List<String>>() {
+    @Override
+    public void onSuccess(List<String> result) {
+        System.out.println(result);
+        ListenableFuture<String> future3 = guavaExecutor.submit(() -> {
+            System.out.println("执行step 3");
+            return "step3 result";
+        });
+        Futures.addCallback(future3, new FutureCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                System.out.println(result);
+            }        
+            @Override
+            public void onFailure(Throwable t) {
             }
-        }
-        return false;
+        }, guavaExecutor);
     }
-}
+
+    @Override
+    public void onFailure(Throwable t) {
+    }}, guavaExecutor);
